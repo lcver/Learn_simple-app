e8a63c5749918f7c0ec62580bd136a7be2c042e3
lcver <crackover1092@gmail.com>
2020-08-06 02:33:45 +0700

    Membuat router homepage

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 
 const app = express();
 
+// membuat router untuk "/"
+app.get("/", function (req, res) {
+    res.end("Hello World")
+})
+
 // nyalain server
 app.listen(3000, function () {
     console.log('Server berjalan diport 3000');
