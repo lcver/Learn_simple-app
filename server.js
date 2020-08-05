e23bb7bbc4a9b49b266c844aa84ff035d716e50e
lcver <crackover1092@gmail.com>
2020-08-06 02:31:50 +0700

    Langkah awal membuat server

---
+++ server.js
@@ -0,0 +1,9 @@
+// ini namanya denpedensi
+const express = require('express');
+
+const app = express();
+
+// nyalain server
+app.listen(3000, function () {
+    console.log('Server berjalan diport 3000');
+})
